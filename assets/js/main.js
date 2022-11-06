7f09987c21ebb61ced9d95a83e87bf89509f369a
Elsti97 <elsti.shehati@hotmail.it>
2022-11-06 18:45:56 +0100

    stampato tutto su DOM

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+// Viene fornito un file js in cui è definito un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// MILESTONE 1:
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// organizzare i singoli membri in card/schede
+
+
+
+const team = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
+
+
+const photo = './assets/img/'
+
+
+for (let i in team) {
+    let user = team[i];
+    console.log(`Name: ${user.name}, Role: ${user.role}, String_img: ${user.image}.`);
+    document.getElementById("person").innerHTML += `
+    <div class="cont mt-5 bg-white">
+      <img src=${photo + user.image}>
+      <div class="fs-5 text-secondary fw-bold pt-3">${user.name}</div>
+      <div>${user.role}</div>
+    </div>`
+}
